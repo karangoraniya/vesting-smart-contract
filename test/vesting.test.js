864d61f8089cd684ef92111263840ff23c6c3878
karangorania <karangoraniya@gmail.com>
2022-05-19 19:15:47 +0530

    more test cases added 🚀

--- test/vesting.test.js
+++ test/vesting.test.js
@@ -3,18 +3,29 @@ const { BigNumber } = require('ethers');
 const { ethers } = require('hardhat');
 
 describe('Vesting', function () {
-  it('Should start the vesting ', async function () {
+  let NappyToken;
+  let nappyToken;
+  let Vesting;
+  let vesting;
+  let addr1;
+  let addr2;
+  let addr3;
+  let addr4;
+  let owner;
+  let creator;
+
+  beforeEach(async () => {
     [owner, creator, addr1, addr2, addr3] = await ethers.getSigners();
-    const NappyToken = await ethers.getContractFactory('NappyToken');
+    NappyToken = await ethers.getContractFactory('NappyToken');
     const totalSupply = BigNumber.from(10000000);
-    const nappyToken = await NappyToken.deploy(totalSupply);
+    nappyToken = await NappyToken.deploy(totalSupply);
     await nappyToken.deployed();
     nappyTokenAddress = nappyToken.address;
 
     // For Vesting Contract
 
-    const Vesting = await hre.ethers.getContractFactory('Vesting');
-    const vesting = await Vesting.deploy(nappyTokenAddress);
+    Vesting = await hre.ethers.getContractFactory('Vesting');
+    vesting = await Vesting.deploy(nappyTokenAddress);
     await vesting.deployed();
     vestingAddress = vesting.address;
 
@@ -26,9 +37,55 @@ describe('Vesting', function () {
     await vesting.connect(owner).addBeneficiary(addr1.address, 0);
     await vesting.connect(owner).addBeneficiary(addr2.address, 1);
     await vesting.connect(owner).addBeneficiary(addr3.address, 2);
+  });
 
+  it('Should start the vesting ', async () => {
     await vesting.startVesting(2 * 2629743, 22 * 2629743);
 
     expect(await vesting.vestingStarted()).to.equal(true);
+  });
+
+  it('Should not claim tokens in cliff period ', async () => {
+    await vesting.startVesting(2 * 2629743, 22 * 2629743);
+
+    expect(vesting.connect(addr1).claimToken()).to.be.revertedWith();
+  });
+
+  it('Should claim tokens after cliff period ', async () => {
+    await vesting.startVesting(2 * 2629743, 22 * 2629743);
+
+    await ethers.provider.send('evm_increaseTime', [
+      2 * 2629743 + 22 * 2629743,
+    ]);
+    const balanceBefore = await nappyToken
+      .connect(addr1)
+      .balanceOf(addr1.address);
+
+    await vesting.connect(addr1).claimToken();
+
+    const balanceAfter = await nappyToken
+      .connect(addr1)
+      .balanceOf(addr1.address);
+
+    expect(balanceBefore).to.be.not.equal(balanceAfter);
+  });
+
+  it('Should claim tokens after cliff period ', async () => {
+    await vesting.startVesting(2 * 2629743, 22 * 2629743);
+
+    await ethers.provider.send('evm_increaseTime', [
+      2 * 2629743 + 22 * 2629743,
+    ]);
+    const balanceBefore = await nappyToken
+      .connect(addr1)
+      .balanceOf(addr1.address);
+
+    await vesting.connect(addr1).claimToken();
+
+    const balanceAfter = await nappyToken
+      .connect(addr1)
+      .balanceOf(addr1.address);
+
+    expect(balanceBefore).to.be.not.equal(balanceAfter);
   });
 });
